3b7e0b3d8d7e643827f0762b008e15842d9d6b4f
piyush yadav <yadavpiyush222@gmail.com>
2023-11-23 00:10:48 +0530

    added assigned issue to prism AND updated patch api to take userId

--- zod/validationSchema.ts
+++ zod/validationSchema.ts
@@ -4,4 +4,15 @@ export const issueSchema = z.object({
   description: z.string().min(1, "description required"),
 });
 
+export const patchIssueSchema = issueSchema.extend({
+  assignedToUserId: z
+    .string()
+    .min(1, "AssignedToUserId is required.")
+    .max(255)
+    .optional()
+    .nullable(),
+});
+
+
 export type IssueForm = z.infer<typeof issueSchema>;
+export type PatchIssue = z.infer<typeof patchIssueSchema>;
