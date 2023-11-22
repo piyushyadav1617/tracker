7632b39ed21a62e0c3000dc178c23db4bc5d1ada
piyush yadav <yadavpiyush222@gmail.com>
2023-11-22 23:25:25 +0530

    fetch data with query in assigneeSelect

--- app/issues/[id]/assigneeSelect.tsx
+++ app/issues/[id]/assigneeSelect.tsx
@@ -11,17 +11,26 @@ import {
 import { User } from "@prisma/client";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { useQuery } from '@tanstack/react-query';
+import { Skeleton } from "@/components/ui/skeleton";
 export function AssigneeSelect() {
-  const [users, setUsers] = useState<User[]>([]);
 
-  useEffect(() => {
-    fetchUsers();
-  }, []);
-  const fetchUsers = async () => {
-    const { data } = await axios.get<User[]>("/api/users");
-    setUsers(data);
+
+  const { data: users, isError,error, isPending, isSuccess} = useQuery<User[]>({
+    queryKey: ['users'],
+    queryFn: () => axios.get('/api/users').then(res=>res.data),
+    staleTime: 60 * 1000, //60s
+    retry: 3
+  });
+
+  if (isPending) return <Skeleton className="w-full h-10 md:w-[200px]"/>;
+
+  if (isError){
+    console.log(error) 
+    return null
   };
-  return (
+
+  if(isSuccess) return (
     <Select>
       <SelectTrigger className="w-full md:w-[200px]">
         <SelectValue placeholder="Assign..." />
@@ -29,7 +38,7 @@ export function AssigneeSelect() {
       <SelectContent>
         <SelectGroup>
           <SelectLabel>Suggestions</SelectLabel>
-          {users.map((user) => (
+          {users?.map((user) => (
             <SelectItem key={user.id} value={user.id}>
               {user.name}
             </SelectItem>
