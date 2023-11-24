d0ffc7400da600914d6e389a25f5d2561c914a97
piyush yadav <yadavpiyush222@gmail.com>
2023-11-25 00:19:33 +0530

    added toast for assigning issue

--- app/issues/[id]/assigneeSelect.tsx
+++ app/issues/[id]/assigneeSelect.tsx
@@ -11,45 +11,71 @@ import {
 import { User, Issue } from "@prisma/client";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useQuery } from '@tanstack/react-query';
+import { useQuery } from "@tanstack/react-query";
 import { Skeleton } from "@/components/ui/skeleton";
-export function AssigneeSelect({issue}:{issue:Issue}) {
+import { useToast } from "@/components/ui/use-toast";
+export function AssigneeSelect({ issue }: { issue: Issue }) {
+  const { toast } = useToast();
 
-
-  const { data: users, isError,error, isPending, isSuccess} = useQuery<User[]>({
-    queryKey: ['users'],
-    queryFn: () => axios.get('/api/users').then(res=>res.data),
+  const {
+    data: users,
+    isError,
+    error,
+    isPending,
+    isSuccess,
+  } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
     staleTime: 60 * 1000, //60s
-    retry: 3
+    retry: 3,
   });
 
-  if (isPending) return <Skeleton className="w-full h-10 md:w-[200px]"/>;
+  if (isPending) return <Skeleton className="w-full h-10 md:w-[200px]" />;
 
-  if (isError){
-    console.log(error) 
-    return null
-  };
-  const assignUser = (userId:string) =>{
-    axios.patch("/api/issues/" + issue.id, {
-      assignedToUserId: userId || null,
-    });
+  if (isError) {
+    console.log(error);
+    return null;
   }
+  const assignUser = (userId: string) => {
+    axios
+      .patch("/api/issues/" + issue.id, {
+        assignedToUserId: userId || null,
+      })
+      .then(() => {
+        toast({
+          description: "Issue assigned to the user",
+          duration: 3000,
+        });
+      })
+      .catch((err) => {
+        toast({
+          title: "Uh oh! Something went wrong.",
+          description: "There was a problem with your request.",
+          variant: "destructive",
+          duration: 3000,
+        });
+      });
+  };
 
-  if(isSuccess) return (
-    <Select defaultValue={issue.assignedToUserId || undefined} onValueChange={(userId)=>assignUser(userId)}>
-      <SelectTrigger className="w-full md:w-[200px]">
-        <SelectValue placeholder="Assign..." />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectLabel>Suggestions</SelectLabel>
-          {users?.map((user) => (
-            <SelectItem key={user.id} value={user.id}>
-              {user.name}
-            </SelectItem>
-          ))}
-        </SelectGroup>
-      </SelectContent>
-    </Select>
-  );
+  if (isSuccess)
+    return (
+      <Select
+        defaultValue={issue.assignedToUserId || undefined}
+        onValueChange={(userId) => assignUser(userId)}
+      >
+        <SelectTrigger className="w-full md:w-[200px]">
+          <SelectValue placeholder="Assign..." />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>Suggestions</SelectLabel>
+            {users?.map((user) => (
+              <SelectItem key={user.id} value={user.id}>
+                {user.name}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    );
 }
